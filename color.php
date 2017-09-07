f4ab4437a357078e08d48107cb9174b0f9d8e498
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-07 16:36:43 +0200

    The validation of color existance is OK

--- color.php
+++ color.php
@@ -1,34 +1,40 @@
-
 <?php
 
-&_GET['color']
+$listeArticles = array (
+'black' => '',
+'white' => '',
+'grey' => '',
+'red' => '',
+'orange' => '',
+'yellow' => '',
+'green' => '',
+'blue' => '',
+'violet' => ''
+);
+
+
+/*Regarder si la couleur possède un article*/
+foreach($listeArticles as $cle => $element) {
+	if (isset($_GET) == $cle) {
+	$colorExist = true;
+} else {
+
+	$colorExist = false;
+
+}
+}//END tableColor
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if () {
+if ($_GET['color'] && $colorExist) {
 /*Si paramètre OK on affiche la page */
-
 ?>
 
 <?php include('header.php');?>
     <div class="container">
       <div class="row">
 	<div class=""></div>
-
+<?php echo $_GET['color'];?>
 
 
      </div>
@@ -40,10 +46,10 @@ if () {
 <?php
 /*Si problème paramètre*/
 } else {
-echo "ERROR !!!"
 
-};
-//END
+echo "ERROR !!!";
+
+}//END issetColor
 
 
 
