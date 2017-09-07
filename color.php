557613543cf869f88d4e11de08f052203de75ed4
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-07 16:06:00 +0200

    The index page is FINISH and make skeleton of color.php page

---
+++ color.php
@@ -0,0 +1,49 @@
+
+<?php
+
+&_GET['color']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if () {
+/*Si paramètre OK on affiche la page */
+
+?>
+
+<?php include('header.php');?>
+    <div class="container">
+      <div class="row">
+	<div class=""></div>
+
+
+
+     </div>
+    </div>
+
+      <hr>
+<?php include('footer.php');?>
+
+<?php
+/*Si problème paramètre*/
+} else {
+echo "ERROR !!!"
+
+};
+//END
+
+
+
