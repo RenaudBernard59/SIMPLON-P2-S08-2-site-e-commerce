184b7608a9cc9ea50407aecb8dc5e643f4d1c6b4
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-08 11:38:36 +0200

    Integring articles in 2Dtab

--- color.php
+++ color.php
@@ -1,7 +1,6 @@
 <?php
-
-$listeArticles = array (
-'black' => "
+$colorsDatabases = array (
+'black' => array('article' => "
 <section>
 <p>Le noir est un champ chromatique regroupant les teintes les plus obscures. Les objets noirs n'émettent ou ne reflètent qu'une part négligeable du spectre de la lumière visible. Le noir s'oppose ainsi à toutes les couleurs, mais surtout au blanc, la plus claire de toutes les couleurs. Certains auteurs, qui prennent le mot « couleur » dans un sens restreint, estiment que le noir n'est pas une couleur.</p>
 <p>La culture occidentale associe principalement le noir au renoncement, involontaire dans le deuil ou volontaire dans la sobriété.</p>
@@ -14,8 +13,9 @@ $listeArticles = array (
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 </section>
-",
-'white' => "
+", 'proprietes' => "0,0,0",'prix' => "20 000 ฿"),
+'white' => array('article' => "
+</section>
 <section>
 <p>Le blanc est un champ chromatique caractérisé par une impression de forte luminosité, sans aucune teinte dominante.</p>
 <p>Pour certains, inspirés des expériences de décomposition et de recomposition de la lumière par Isaac Newton, le blanc n'est pas une couleur, mais « c'est la teinte obtenue en mélangeant la lumière de toutes les couleurs ». Pour d'autres, et suivant les théories de Goethe, de Hering et de nombreux autres, le blanc opposé au noir est une des trois paires de couleurs pures qui orientent la perception visuelle.</p>
@@ -28,9 +28,8 @@ $listeArticles = array (
 <p>Integer blandit lorem et velit accumsan porttitor. Maecenas eleifend, sapien vel consequat tempus, mauris augue laoreet nisi, et semper quam sem eu libero. Ut viverra pharetra elit tincidunt porttitor. Vestibulum ante ipsum, molestie sed orci nec, lobortis lacinia lacus. Nunc purus metus, accumsan quis ornare pretium, dictum a sapien. Nam eget urna ut felis accumsan vehicula eu eu ante. Pellentesque vitae libero est. Quisque felis metus, imperdiet eget elit non, fermentum fermentum est. Praesent eget euismod nibh. Sed consectetur congue justo, vel feugiat eros varius a. Donec dignissim sem consequat lorem iaculis scelerisque.</p>
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
-</section>
-",
-'grey' => "
+", 'proprietes' => "255,255,255",'prix' => "50 000 ฿"),
+'grey' => array('article' => "
 <section>
 <p>Le gris est un champ chromatique défini par la faiblesse ou l'absence de l'impression de couleur, dont l'intensité lumineuse est moindre que celle du blanc et supérieure à celle du noir.</p>
 <p>Les images photographiques ne présentant aucune impression de couleur sont généralement dites en noir et blanc. En peinture, on parle de grisaille1. Le lavis d'encre de Chine sert fréquemment pour produire des monochromes gris.</p>
@@ -43,14 +42,13 @@ $listeArticles = array (
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 </section>
-",
-'red' => "
+", 'proprietes' => "128,128,128",'prix' => "5 000 ฿"),
+'red' => array('article' => "
 <section>
 <p>Le rouge est un champ chromatique regroupant les couleurs vives situées sur le cercle chromatique entre l'orange et les pourpres. Lavé de blanc, le rouge devient rose, sombre, il s'appelle brun. Opposé au vert, il forme un des contrastes qui, avec celui entre le bleu et le jaune et celui entre le noir et le blanc, orientent la perception visuelle. Un rouge, un vert et un bleu suffisent pour la synthèse additive des couleurs ; différentes nuances de rouge peuvent servir de couleur primaire. Pour la théorie ondulatoire de la lumière, la bande rouge est à l'extrémité de moins d'énergie du spectre visible, à la limite de l'infrarouge.</p>
 <p>Certaines cultures classent les sensations visuelles entre le sombre, dont le maximum est le noir, le clair, dont le maximum est le blanc, et le coloré, correspondant aux couleurs vives, dont le maximum est le rouge. Toutes les autres couleurs se placent dans ces catégories fondamentales. Même dans la culture occidentale, où la classification des couleurs par celles de l'arc-en-ciel domine, le rouge vif a un statut particulier, dans la signalisation et dans la symbolique.</p>
 </section>
 <section>
-
 <h3>Lorem ipsum dorloris mortel</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at dui vitae purus lacinia pulvinar sed quis ex. Nunc ullamcorper velit eget justo euismod, ut aliquam lectus convallis. Nam vitae ante molestie, egestas orci sed, condimentum ipsum. Donec nec augue aliquet, cursus diam et, feugiat diam. Suspendisse nec metus et nibh pellentesque rutrum. Integer nec metus sed arcu efficitur pellentesque vitae condimentum erat. Donec eu ipsum id velit imperdiet venenatis. Aliquam diam mi, varius et ornare sed, laoreet vitae lectus. Sed ultrices egestas aliquet. Etiam sagittis eros justo, ut fermentum nunc molestie ut. Aliquam sit amet rhoncus est.</p>
 <p>Phasellus ultrices commodo euismod. Etiam elementum quam vel nisi vulputate, et varius nulla facilisis. Aenean pharetra nunc ac metus mollis, id imperdiet nisl convallis. Curabitur vitae justo in felis finibus posuere. Etiam auctor a nunc eu rutrum. Sed arcu dui, suscipit non libero et, auctor sollicitudin felis. Cras malesuada ac magna quis finibus. Praesent id facilisis justo, et posuere quam.</p>
@@ -58,8 +56,8 @@ $listeArticles = array (
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 </section>
-",
-'orange' => "
+", 'proprietes' => "255,0,0",'prix' => " 30 000 ฿"),
+'orange' => array('article' => "
 <section>
 <p>L'orange est un champ chromatique de couleurs vives qui, sur le cercle chromatique, prennent place entre le rouge et le jaune.</p>
 <p>Les couleurs orangées sont des couleurs chaudes ; les couleurs chaudes se définissent par proximité avec l'orange. Elles sont considérées comme stimulantes et provocantes.</p>
@@ -72,8 +70,8 @@ $listeArticles = array (
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 </section>
-",
-'yellow' => "
+", 'proprietes' => "255,156,0",'prix' => "30 000 ฿"),
+'yellow' => array('article' => "
 <section>
 <p>Le jaune est un champ chromatique correspondant à des couleurs claires situées entre l'orange et le vert. Une des trois couleurs primaires de la synthèse soustractive des couleurs est un jaune.</p>
 <p>En héraldique, le jaune s'appelle or.</p>
@@ -86,8 +84,9 @@ $listeArticles = array (
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 </section>
-",
-'green' => "
+", 'proprietes' => "255,255,0",'prix' => "20 000 ฿"),
+'green' => array('article' => "
+</section>
 <section>
 <p>Le vert est un champ chromatique regroupant les couleurs situées sur le cercle chromatique entre le jaune et le bleu. Contrairement à d'autres couleurs, qui changent de nom quand elles sont lavées de blanc ou rabattues avec du noir, comme le rouge qui devient rose ou brun, le vert conserve son nom, vert pâle ou vert foncé, vert vif ou vert grisâtre.</p>
 <p>Le vert dû à la chlorophylle est la couleur de la plupart des feuillages de la végétation.</p>
@@ -99,9 +98,9 @@ $listeArticles = array (
 <p>Integer blandit lorem et velit accumsan porttitor. Maecenas eleifend, sapien vel consequat tempus, mauris augue laoreet nisi, et semper quam sem eu libero. Ut viverra pharetra elit tincidunt porttitor. Vestibulum ante ipsum, molestie sed orci nec, lobortis lacinia lacus. Nunc purus metus, accumsan quis ornare pretium, dictum a sapien. Nam eget urna ut felis accumsan vehicula eu eu ante. Pellentesque vitae libero est. Quisque felis metus, imperdiet eget elit non, fermentum fermentum est. Praesent eget euismod nibh. Sed consectetur congue justo, vel feugiat eros varius a. Donec dignissim sem consequat lorem iaculis scelerisque.</p>
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
-</section>
-",
-'blue' => "
+", 'proprietes' => "0,255,0",'prix' => "20 000 ฿"),
+'blue' => array('article' => "
+<p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 <section>
 <p>Le bleu est un champ chromatique, regroupant les teintes rappelant celles du ciel ou de la mer par temps claira.</p>
 <p>On rencontre des pierres, des oiseaux, des fleurs et des papillons bleus, mais les matières bleues sont moins fréquentes dans la nature que les vertes, les rouges et les jaunes.</p>
@@ -112,10 +111,9 @@ $listeArticles = array (
 <p>Phasellus ultrices commodo euismod. Etiam elementum quam vel nisi vulputate, et varius nulla facilisis. Aenean pharetra nunc ac metus mollis, id imperdiet nisl convallis. Curabitur vitae justo in felis finibus posuere. Etiam auctor a nunc eu rutrum. Sed arcu dui, suscipit non libero et, auctor sollicitudin felis. Cras malesuada ac magna quis finibus. Praesent id facilisis justo, et posuere quam.</p>
 <p>Integer blandit lorem et velit accumsan porttitor. Maecenas eleifend, sapien vel consequat tempus, mauris augue laoreet nisi, et semper quam sem eu libero. Ut viverra pharetra elit tincidunt porttitor. Vestibulum ante ipsum, molestie sed orci nec, lobortis lacinia lacus. Nunc purus metus, accumsan quis ornare pretium, dictum a sapien. Nam eget urna ut felis accumsan vehicula eu eu ante. Pellentesque vitae libero est. Quisque felis metus, imperdiet eget elit non, fermentum fermentum est. Praesent eget euismod nibh. Sed consectetur congue justo, vel feugiat eros varius a. Donec dignissim sem consequat lorem iaculis scelerisque.</p>
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
-<p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
 </section>
-",
-'violet' => "
+", 'proprietes' => "0,0,255",'prix' => "30 000 ฿"),
+'violet' => array('article' => "
 <p>Le violet est un champ chromatique qui désigne des couleurs situées sur le cercle chromatique entre les pourpres et les bleus.</p>
 </section>
 <section>
@@ -125,21 +123,8 @@ $listeArticles = array (
 <p>Integer blandit lorem et velit accumsan porttitor. Maecenas eleifend, sapien vel consequat tempus, mauris augue laoreet nisi, et semper quam sem eu libero. Ut viverra pharetra elit tincidunt porttitor. Vestibulum ante ipsum, molestie sed orci nec, lobortis lacinia lacus. Nunc purus metus, accumsan quis ornare pretium, dictum a sapien. Nam eget urna ut felis accumsan vehicula eu eu ante. Pellentesque vitae libero est. Quisque felis metus, imperdiet eget elit non, fermentum fermentum est. Praesent eget euismod nibh. Sed consectetur congue justo, vel feugiat eros varius a. Donec dignissim sem consequat lorem iaculis scelerisque.</p>
 <p>Fusce posuere bibendum nunc vel suscipit. Suspendisse non turpis eget enim mollis rutrum. Pellentesque id faucibus urna. Nulla felis quam, feugiat a venenatis ac, pretium gravida eros. Integer ultricies elit vitae enim pellentesque imperdiet. Sed tristique varius justo quis ultricies. Quisque placerat lobortis arcu eget consequat. Donec nec dolor metus.</p>
 <p>Nulla consectetur vel quam et convallis. Vivamus vitae lectus ligula. Duis congue augue lectus, in porttitor purus semper id. Aliquam tincidunt dolor in vulputate imperdiet. Sed placerat neque in nulla suscipit consequat egestas a arcu. Morbi sollicitudin mattis sem ut volutpat. Vestibulum malesuada lectus at risus malesuada tincidunt. Sed commodo, nisi sit amet tempor pretium, neque tellus sagittis dui, tempus semper lacus risus vitae lectus. Praesent vitae est felis.</p>
-</section>
-"
-);
-
-$colorsDatabases = array (
-'black' => array('article' => "###", 'proprietes' => "0,0,0",'prix' => "20 000 ฿"),
-'white' => array('article' => "###", 'proprietes' => "255,255,255",'prix' => "50 000 ฿"),
-'grey' => array('article' => "###", 'proprietes' => "128,128,128",'prix' => "5 000 ฿"),
-'red' => array('article' => "###", 'proprietes' => "255,0,0",'prix' => " 30 000 ฿"),
-'orange' => array('article' => "###", 'proprietes' => "255,156,0",'prix' => "30 000 ฿"),
-'yellow' => array('article' => "###", 'proprietes' => "255,255,0",'prix' => "20 000 ฿"),
-'green' => array('article' => "###", 'proprietes' => "0,255,0",'prix' => "20 000 ฿"),
-'blue' => array('article' => "###", 'proprietes' => "0,0,255",'prix' => "30 000 ฿"),
-'violet' => array('article' => "###", 'proprietes' => "238,130,238",'prix' => "5 000 ฿")
-);
+</section>", 'proprietes' => "238,130,238",'prix' => "5 000 ฿")
+	);
 
 /*Regarder si la couleur possède un article*/
 foreach($listeArticles as $cle => $element) {
