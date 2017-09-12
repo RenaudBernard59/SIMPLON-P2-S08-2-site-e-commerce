1de1cfd8372db5490ec20ea77727b44375520899
RenaudBernard59 <renaud.bernard@p59production.com>
2017-09-12 11:49:53 +0200

    END ! Adding indexDB + make php generation for index articles-button

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
-<?php include('header.php');?>
+<?php
+//#######################################################################################
+include('indexDB.php');
+
+$avaiableColors = array('black', 'white', 'grey', 'red', 'orange', 'yellow', 'green', 'blue', 'violet');
+
+function indexGenrator($indexDatabases, $selectedColors) {
+/*###############################################
+# Définition de la fonction de la génération des liens vers les articles
+#
+#
+################################################*/
+  echo '<div class="col-md-4">';
+  echo "<h2>" . $selectedColors . "</h2>";
+  echo '<img src="img/' . $selectedColors . '.png" alt="' . $selectedColors . '" class="col-md-12" />'; 
+  echo "<p>" . $indexDatabases[$selectedColors]['article'][0] . "</p>";
+  echo "<p>" . $indexDatabases[$selectedColors]['article'][1] . '<srong>' . $indexDatabases[$selectedColors]['article'][2] . "</strong></p>";
+  echo '<p><a class="btn btn-default" href="color.php?color=' . [$selectedColors] . '" role="button">View details &raquo;</a></p>';
+  echo  "</div>";
+
+}//END indexGenrator
+
+
+/*================================================
+Appel fonctions*/
+//GENRATION DE LA PAGE
+/*=========================================================================
+======
+=.......................... Inclusion de la page
+======
+==========================================================================*/
+
+include('header.php');?>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
@@ -9,75 +41,33 @@
         <p><a class="btn btn-primary btn-lg" href="#" role="button">J'en veux plus &raquo;</a></p>
       </div>
     </div>
+<!--/*==================================*/-->
+<div class="container">
+  <div class="row">
+<?php
+for ($i = 0 ; $i < (count($indexDatabases)) ; $i++) {
+    indexGenrator($indexDatabases, $avaiableColors[$i]);
+}
+?>
 
-    <div class="container">
-      <div class="row">
+<!--/*==================================*/-->
+  </div>
+</div> 
+<hr>
+
+<?php include('footer.php');
+
+/*=========================================================================
+======
+=.......................... FIN Inclusion de la page
+======
+==========================================================================*/
+//#######################################################################################
+/*
         <div class="col-md-4">
           <h2>Blue</h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-default" href="color.php?color=blue" role="button">View details &raquo;</a></p>
         </div>
-        <div class="col-md-4">
-          <h2>Red</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            
-          <p><a class="btn btn-default" href="color.php?color=red" role="button">View details &raquo;</a></p>
-
-        </div>
-        <div class="col-md-4">
-          <h2>Yellow</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="color.php?color=yellow" role="button">View details &raquo;</a></p>
-        </div>
-
-
-<!--/*==================================*/-->
-
-
-        <div class="col-md-4">
-          <h2>Green</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="color.php?color=green" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Orange</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="color.php?color=orange" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Violet</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="color.php?color=violet" role="button">View details &raquo;</a></p>
-        </div>
-
-
-<!--/*==================================*/-->
-
-
-        <div class="col-md-4">
-          <h2>Black</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="color.php?color=black" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Grey</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="color.php?color=grey" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>White</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="color.php?color=white" role="button">View details &raquo;</a></p>
-        </div>
-
-
-<!--/*==================================*/-->
-
-
-
-      </div>
-  
-      <hr>
-
-
-<?php include('footer.php');?>
+*/
+?>
